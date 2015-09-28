d452f40c5e49493f18d573bcdb1c376022c0c5c5
xuyixuan <yixxu916@student.liu.se>
2015-09-28 23:42:01 +0200

    little change

--- server.R
+++ server.R
@@ -4,15 +4,15 @@ library("RCurl")
 library("jsonlite")
 
 shinyServer(function(input, output) {
-  data1 <-geocode_GET("sweden")
-  data2 <-geocode_GET("usa")
-  data3 <-geocode_GET("spain")
+#   data1 <-geocode_GET("sweden")
+#   data2 <-geocode_GET("usa")
+#   data3 <-geocode_GET("spain")
   # Return the requested loadAPI
   loadAPIInput <- reactive({
     switch(input$geocode_GET,
-           "sweden" = data1,
-           "usa" = data2,
-           "spain" = data3)
+           "sweden" = geocode_GET("sweden"),
+           "usa" = geocode_GET("usa"),
+           "spain" = geocode_GET("spain"))
   })
   
 
