aba17ebf83b68c096851594a6ca071e2e0e19bfe
xuyixuan <xuyixuan@users.noreply.github.com>
2015-10-06 12:52:01 +0200

    Update server.R

--- server.R
+++ server.R
@@ -1,8 +1,8 @@
 library("shiny")
 library("bitops")
-library("RCurl", lib.loc="~/R/win-library/3.2")
-library("jsonlite", lib.loc="~/R/win-library/3.2")
-library("devtools", lib.loc="~/R/win-library/3.2")
+library("RCurl")
+library("jsonlite")
+library("devtools")
 devtools::install_github("xuyixuan/lab5/loadAPI")
 library("loadAPI")
 
